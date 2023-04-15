3d724a520df3ad1a2363d5beed8325ecad0b27ba
Yaman Kalaji <syk.yaman@gmail.com>
2023-04-15 18:42:14 +0100

    Add InfluxDB client JS library to make first connection

--- Code/web_api/services/exampleService.js
+++ Code/web_api/services/exampleService.js
@@ -1,6 +1,23 @@
 //Import other services within this app
+import {InfluxDB} from '@influxdata/influxdb-client'
+import {url, token, org} from '../env.mjs'
 
-export default async function getTestString(){
+const queryApi = new InfluxDB({url, token}).getQueryApi(org)
+const fluxQuery =
+  'from(bucket:"my-bucket") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "temperature")'
+
+export default async function getExample(){
+
+    collectRows();
 
     return "test";
 }
+
+async function collectRows() {
+    console.log('\n*** CollectRows ***')
+    const data = await queryApi.collectRows(
+      fluxQuery //, you can also specify a row mapper as a second argument
+    )
+    data.forEach((x) => console.log(JSON.stringify(x)))
+    console.log('\nCollect ROWS SUCCESS')
+  }
