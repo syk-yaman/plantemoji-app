fd1c5f79ad2108f9077418536ed07707cd5e29f0
Yaman Kalaji <syk.yaman@gmail.com>
2023-04-22 23:47:36 +0100

    Add a general colors class for the entire app

---
+++ Code/plantemoji_app/lib/assets/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// usage: Container(color: AppColors.container)
+class AppColors {
+  static const Color blueFont = Color.fromARGB(255, 0, 102, 255);
+  static const Color greyFont = Color.fromARGB(255, 109, 109, 109);
+  static const Color greenFont = Color.fromARGB(255, 78, 204, 82);
+  static Color text = Colors.white;
+  static Color textDark = Colors.white38;
+}
