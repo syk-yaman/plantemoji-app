37e79bbeddc47ba54e9d9cce66b401239dff4a99
Yaman Kalaji <syk.yaman@gmail.com>
2023-04-24 07:04:55 +0100

    Add the QR reader to the Setup Device page

--- Code/plantemoji_app/lib/assets/app_colors.dart
+++ Code/plantemoji_app/lib/assets/app_colors.dart
@@ -6,5 +6,6 @@ class AppColors {
   static const Color greyFont = Color.fromARGB(255, 109, 109, 109);
   static const Color greenFont = Color.fromARGB(255, 78, 204, 82);
   static const Color blackFont = Color.fromARGB(255, 46, 46, 46);
+  static const Color redFont = Color.fromARGB(255, 172, 38, 38);
   static const Color whiteFont = Colors.white;
 }
